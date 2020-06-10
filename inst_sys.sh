1f26dcbe126137872b1123eea357cbe88f059d29
DoubleDashes <18100602+DoubleDashes@users.noreply.github.com>
2020-06-11 01:49:02 +0200

    Update and rename install_system.sh to inst_sys.sh

--- inst_sys.sh
+++ inst_sys.sh
@@ -8,7 +8,7 @@ echo "Installing XServer..."
 apt -y install xserver-xorg-video-all xserver-xorg-input-all xserver-xorg-core xinit x11-xserver-utils
 
 echo "Configuring XServer..."
-wget -O /home/kiosk-user/.xinitrc https://gist.githubusercontent.com/TheWhoosher/2ef8a8e3c2fc902d000061a9ef89f73d/raw
+wget -O /home/kiosk-user/.xinitrc https://raw.githubusercontent.com/KioskPEM/CactusInstall/master/.xinitrc
 chown kiosk-user /home/kiosk-user/.xinitrc
 echo "startx"> /home/kiosk-user/.bash_profile
 
@@ -21,7 +21,7 @@ apt -y install nginx
 echo "Configuring Nginx..."
 rm /etc/nginx/sites-available/default
 rm /etc/nginx/sites-enabled/default
-wget -O /etc/nginx/sites-available/cactus.conf https://gist.githubusercontent.com/TheWhoosher/bc495cd01e97f485b5cf01b1d6b95245/raw
+wget -O /etc/nginx/sites-available/cactus.conf https://raw.githubusercontent.com/KioskPEM/CactusInstall/master/cactus.conf
 ln -s /etc/nginx/sites-available/cactus.conf /etc/nginx/sites-enabled/
 service nginx reload
 
@@ -32,7 +32,7 @@ apt update
 apt -y install php7.4-fpm php7.4-curl php7.4-zip php7.4-mbstring
 
 echo "Downloading Cactus..."
-wget -O /home/kiosk-user/master.zip https://github.com/TheWhoosher/Cactus/archive/master.zip
+wget -O /home/kiosk-user/master.zip https://github.com/KioskPEM/Cactus/archive/master.zip
 
 echo "Decompressing Cactus..."
 unzip /home/kiosk-user/master.zip -d /home/kiosk-user
@@ -59,6 +59,6 @@ sleep 2
 modprobe pcspkr
 
 echo "Downloading additional scripts..."
-wget -O /home/kiosk-user/inst_ts.sh https://gist.githubusercontent.com/TheWhoosher/a59e16997b3614dba6492ed06354e0f7/raw
+wget -O /home/kiosk-user/inst_ts.sh https://raw.githubusercontent.com/KioskPEM/CactusInstall/master/inst_ts.sh
 
 echo "Done."
